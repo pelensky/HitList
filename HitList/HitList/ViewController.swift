39e274b9c372091a41d8846573e391cfea1d92ad
Dan Pelensky <dan@pelensky.com>
2017-01-31 16:48:14 +0000

    UITableView and UIBarButton Item referred to in code

--- HitList/HitList/ViewController.swift
+++ HitList/HitList/ViewController.swift
@@ -10,14 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func addName(_ sender: UIBarButtonItem) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
